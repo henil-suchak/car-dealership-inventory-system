aa00e1f0ce1b1d77ec1c95791942ad9b724bf3a4
henil suchak <henilsuchak8311@gmail.com>
2026-07-12 11:06:55 +0530

    Style(MainLayout): tweak layout dimensions [REFACTOR]

--- car-dealership-web/src/components/layout/MainLayout.jsx
+++ car-dealership-web/src/components/layout/MainLayout.jsx
@@ -4,9 +4,9 @@ import Navbar from './Navbar';
 
 const MainLayout = () => {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col pt-16">
+    <div className="min-h-screen bg-gray-50 flex flex-col pt-16 relative">
       <Navbar />
-      <main className="flex-grow flex flex-col">
+      <main className="flex-grow flex flex-col w-full mx-auto">
         <Outlet />
       </main>
     </div>
