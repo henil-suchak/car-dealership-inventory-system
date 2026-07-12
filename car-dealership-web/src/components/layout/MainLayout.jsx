b31c8b879c7397b7c49d6587c1c658c99c6d1099
henil suchak <henilsuchak8311@gmail.com>
2026-07-12 11:06:29 +0530

    Feat(MainLayout): implement MainLayout component to pass tests [GREEN]

---
+++ car-dealership-web/src/components/layout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const MainLayout = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col pt-16">
+      <Navbar />
+      <main className="flex-grow flex flex-col">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default MainLayout;
